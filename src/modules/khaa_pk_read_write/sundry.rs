4fa07a6bb1c549adb1a83b5c2ab802bccde45f80
KHAAdotPK <spysee.pk@gmail.com>
2025-03-14 01:32:42 +0500

    revert: restore `bitmap_font` FFI code in sundry.rs
    - Uncommented the `bitmap_font` FFI declaration and its unsafe call in `sundry.rs`.
    - Restored the functionality for `bitmap_font` as it is now required again.

--- src/modules/khaa_pk_read_write/sundry.rs
+++ src/modules/khaa_pk_read_write/sundry.rs
@@ -8,13 +8,10 @@ use crate::modules::{model::{content::{Content, ContentBody}, dict::{Dict, DictB
 
 use super::read_write::{read, exists};
 
-/*
 extern {
     
     fn bitmap_font(pixels: *mut u8, height: u32, width: u32);    
 }
- */    
-
 
 pub fn get_dict(content: &Content) -> /*Vec<Vec<String>>*/ Dict {
 
@@ -184,7 +181,7 @@ pub fn handle_connection(mut stream: TcpStream, config_dict: &Dict) {
 
     let mut content = Content {content: Vec::new(), content_length: 0};
 
-    /*unsafe { bitmap_font( core::ptr::null_mut(), 0, 0); }*/
+    unsafe { bitmap_font( core::ptr::null_mut(), 0, 0); }
 
     let document_root = config_dict.find("DocumentRoot");
     if !(document_root.len() > 1) {
